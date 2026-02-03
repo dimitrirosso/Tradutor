71cb5849246f0b2a7b0f90127a4a579f647d8b7c
Dimitri <135258757+dimitrirosso@users.noreply.github.com>
2026-02-03 15:44:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+/*https://api.mymemory.translated.net/get?q=*/
+
+/*Hello World!*/
+
+/*&langpair=pt-BR|en*/
+
+let texto = document.querySelector(".input-texto")
+let traducao = document.querySelector(".traducao")
+let idioma = document.querySelector(".idioma")
+
+async function traduzir(){
+
+    let endereco = "https://api.mymemory.translated.net/get?q=" + texto.value + "&langpair=pt-BR|" + idioma.value;
+
+    let resposta = await fetch(endereco);
+
+    let dados = await resposta.json();
+    traducao.innerHTML = dados.responseData.translatedText
+
+}
+
+
+function Ouvirvoz(){
+    let voz = window.webkitSpeechRecognition;
+
+    let reconhecimentoVoz = new voz();
+    reconhecimentoVoz.lang = "pt-BR";
+
+    reconhecimentoVoz.onresult = (evento) => {
+        let TextoTraduzido = evento.results[0][0].transcript;
+
+        texto.innerHTML = TextoTraduzido;
+        traduzir()
+
+    }
+
+    reconhecimentoVoz.start();
+
+}
